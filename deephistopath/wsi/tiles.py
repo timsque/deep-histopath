a91619cc5b20c5a760d72d89124e558306ef5fc3
timsque <timsque@gmail.com>
2020-12-31 11:45:58 -0700

    Update tiles.py

--- deephistopath/wsi/tiles.py
+++ deephistopath/wsi/tiles.py
@@ -37,7 +37,7 @@ TISSUE_LOW_THRESH = 10
 
 ROW_TILE_SIZE = 1024
 COL_TILE_SIZE = 1024
-NUM_TOP_TILES = 50
+NUM_TOP_TILES = 1000
 
 DISPLAY_TILE_SUMMARY_LABELS = False
 TILE_LABEL_TEXT_SIZE = 10
@@ -518,12 +518,10 @@ def summary_and_tiles(slide_num, display=True, save_summary=False, save_data=Tru
   generate_tile_summaries(tile_sum, np_img, display=display, save_summary=save_summary)
   generate_top_tile_summaries(tile_sum, np_img, display=display, save_summary=save_summary)
   if save_top_tiles:
-    #modified below to save all tiles above low tissue threshold
-    for tile in tile_sum.tiles_by_tissue_percentage():
+    #modified below to save more tiles above low tissue threshold
+    for tile in tile_sum.top_tiles():
       if tile.tissue_percentage > TISSUE_LOW_THRESH:
         tile.save_tile()
-    #for tile in tile_sum.top_tiles():
-      #tile.save_tile()
   return tile_sum
 
 
